3850de823f2812d3813c30eb3457069803335deb
Justin Morgan <2justinmorgan@gmail.com>
2023-02-08 16:18:47 -0800

    Create func get_unique_num

--- tools/shell/testing/common/functions.sh
+++ tools/shell/testing/common/functions.sh
@@ -5,6 +5,10 @@ function exit_err_test_fail() {
 	exit 1
 }
 
+function get_unique_num() {
+	echo "${RANDOM}${RANDOM}" | xargs | tr ' ' '0'
+}
+
 # used to verify the contents of this file have been sourced
 function check_sourced_functions_testing() {
 	:
