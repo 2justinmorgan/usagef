13c7758235417f045312a6fc9769324c7e58ce4c
Justin Morgan <2justinmorgan@gmail.com>
2023-02-09 17:14:57 -0800

    Add common shell tool strings

---
+++ tools/shell/common/strings.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DIR_TESTS="tests"
+export DIR_TESTS_MISC="${DIR_TESTS}/misc"
+
+# used to verify the contents of this file have been sourced
+function check_sourced_strings() {
+	:
+}
--- tools/shell/testing/test_misc.sh
+++ tools/shell/testing/test_misc.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
-function test_is_valid_branch_name() {
-	if ./tests/misc/test_is_valid_branch_name.sh; then return; fi
-	echo >&2 "an is_valid_branch_name test failed"
+. tools/shell/common/functions.sh
+. tools/shell/common/strings.sh
+
+function test_misc() {
+	local misc_test_script_name="$1"
+	local err_msg="$2"
+
+	if "${DIR_TESTS_MISC}/$misc_test_script_name"; then
+		return
+	fi
+
+	echo >&2 "$err_msg"
 	exit 1
 }
 
 function test_misc_all() {
 	echo "testing all misc tests"
-	test_is_valid_branch_name
+
+	test_misc \
+		test_is_valid_branch_name.sh \
+		"a branch name is not valid"
+
+	test_misc \
+		test_shell_common_strings.sh \
+		"a common shell tool string does not have an expected value"
+}
+
+function check_sources() {
+	check_sourced_functions || exit 1
+	check_sourced_strings || exit_err
 }
 
 function main() {
+	check_sources
 	test_misc_all
 }
 
